3b0d012cc323f9a9d37819a05e6e4fb0be7b389b
umfrancisco <francis.guitler@gmail.com>
2026-03-24 08:42:03 -0300

    update projects

--- hibernate/HibernateProject_HQL/src/main/java/com/umfrancisco/Main.java
+++ hibernate/HibernateProject_HQL/src/main/java/com/umfrancisco/Main.java
@@ -1,69 +1,33 @@
 package com.umfrancisco;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class Main {
 	public static void main(String[] args) {
-		Laptop l1 = new Laptop();
-		l1.setId(1);
-		l1.setBrand("Asus");
-		l1.setModel("Rog");
-		l1.setRam(16);
-		
-		Laptop l2 = new Laptop();
-		l2.setId(2);
-		l2.setBrand("Dell");
-		l2.setModel("XPS");
-		l2.setRam(32);
-		
-		Laptop l3 = new Laptop();
-		l3.setId(3);
-		l3.setBrand("Apple");
-		l3.setModel("Macbook air");
-		l3.setRam(8);
-		
-		Alien a1 = new Alien();
-		a1.setId(101);
-		a1.setName("Navin");
-		a1.setTech("Java");
-		
-		Alien a2 = new Alien();
-		a2.setId(102);
-		a2.setName("Harsh");
-		a2.setTech("Python");
-		
-		a1.setLaptops(Arrays.asList(l1, l2));
-		a2.setLaptops(Arrays.asList(l3));
 		
 		SessionFactory sf = new Configuration()
-				.addAnnotatedClass(com.umfrancisco.Alien.class)
 				.addAnnotatedClass(com.umfrancisco.Laptop.class)
 				.configure()
 				.buildSessionFactory();
 		
 		Session session = sf.openSession();
-		Transaction transaction = session.beginTransaction();
 		
-		session.persist(l1);
-		session.persist(l2);
-		session.persist(l3);
-		session.persist(a1);
-		session.persist(a2);
+		// select * from laptop where ram = 32 -> SQL
+		// from Laptop where ram = 32 -> HQL
 		
-		transaction.commit();
+		Query<Laptop> query = session.createQuery("from Laptop", Laptop.class);
+		List<Laptop> laptops = query.getResultList();
+		
+		for (Laptop l : laptops) {
+			System.out.println(l);
+		}
 		
 		session.close();
-		
-		Session session1 = sf.openSession();
-		Alien a5 = session1.find(Alien.class, 101);
-//		System.out.println(a5);
-		
-		session1.close();
 		sf.close();
 	}
 }
