af4930f4cab40d6c6d18b1f51cb1a1951f29353c
umfrancisco <francis.guitler@gmail.com>
2026-03-26 09:23:31 -0300

    update projects

---
+++ spring-intro/SpringProject3/src/main/java/com/umfrancisco/Laptop.java
@@ -0,0 +1,12 @@
+package com.umfrancisco;
+
+public class Laptop {
+	
+	public Laptop() {
+		System.out.println("Laptop Object created");
+	}
+	
+	public void compile() {
+		System.out.println("Compiling");
+	}
+}
