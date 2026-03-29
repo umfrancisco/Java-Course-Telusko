2827c0560b68e110de7ccad0a0cbfc04393f9d87
umfrancisco <francis.guitler@gmail.com>
2026-03-29 09:38:56 -0300

    update projects

--- section10-intro-springboot/SpringJDBC/src/main/java/com/umfrancisco/SpringJdbcApplication.java
+++ section10-intro-springboot/SpringJDBC/src/main/java/com/umfrancisco/SpringJdbcApplication.java
@@ -1,4 +1,4 @@
-package com.umfrancisco.SpringJDBC;
+package com.umfrancisco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
