bb68640df33a7f0b76f57c80bfc2b8f9cd8fa8fc
umfrancisco <francis.guitler@gmail.com>
2026-04-03 16:00:33 -0300

    update projects

--- section12-springboot-web/SpringBootWeb1/src/main/java/com/umfrancisco/HomeController.java
+++ section12-springboot-web/SpringBootWeb1/src/main/java/com/umfrancisco/HomeController.java
@@ -2,24 +2,22 @@ package com.umfrancisco;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "index.jsp";
+		return "index";
 	}
 	
 	@RequestMapping("/add")
-	public String add(HttpServletRequest req, HttpSession session) {
-		int num1 = Integer.parseInt(req.getParameter("num1"));
-		int num2 = Integer.parseInt(req.getParameter("num2"));
+	public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv) {
 		int result = num1 + num2;
-		session.setAttribute("result", result);
-		return "result.jsp";
+		mv.addObject("result", result);
+		mv.setViewName("result");
+		return mv;
 	}
 }
