35aa76124dba71e280a8e1c497ad563f4d344459
umfrancisco <francis.guitler@gmail.com>
2026-04-12 16:39:01 -0300

    update projects

--- section15-rest-using-spring/SpringBootRest/src/main/java/com/umfrancisco/controller/JobRestController.java
+++ section15-rest-using-spring/SpringBootRest/src/main/java/com/umfrancisco/controller/JobRestController.java
@@ -1,5 +1,20 @@
 package com.umfrancisco.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.umfrancisco.model.JobPost;
+import com.umfrancisco.service.JobService;
+
+@RestController
 public class JobRestController {
 	
+	@Autowired
+	private JobService service;
+	
+	@GetMapping("posts")
+	public List<JobPost> getAllJobs() {
+		return service.getAllJobs(); 
+	}
 }
--- section15-rest-using-spring/SpringBootRest/src/main/java/com/umfrancisco/repository/JobRepository.java
+++ section15-rest-using-spring/SpringBootRest/src/main/java/com/umfrancisco/repository/JobRepository.java
@@ -10,7 +10,7 @@ import com.umfrancisco.model.JobPost;
 public class JobRepository {
 	
 	List<JobPost> jobs = new ArrayList<>(Arrays.asList(
-            new JobPost(1, "Java Developer", "Must have good experience in core Java and advanced Java", 2,
+            new JobPost(1, "Java Backend Developer", "Must have good experience in core Java and advanced Java", 2,
                     List.of("Core Java", "J2EE", "Spring Boot", "Hibernate")),
             new JobPost(2, "Frontend Developer", "Experience in building responsive web applications using React", 3,
                     List.of("HTML", "CSS", "JavaScript", "React")),
@@ -28,9 +28,5 @@ public class JobRepository {
 	
 	public void addJob(JobPost job) {
 		jobs.add(job);
-		System.out.println("# JOBS");
-		for (var j : jobs) {
-			System.out.println(j);
-		}
 	}
 }
